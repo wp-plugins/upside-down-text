ceab61dbf657dc808cf87dc8a4364f29687e230d
pulegium <pulegium@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-17 15:04:49 +0000

    Adding initial files
    git-svn-id: https://plugins.svn.wordpress.org/upside-down-text/trunk@174607 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,49 @@
+=== Upside Down Text ===
+Contributors: pulegium
+Donate link: http://www.grenadepod.com/projects/upside-down-text-plugin-for-wordpress/
+Tags: post, text, convert, upsidedown, reverse, mirror, plugin, filter, unicode
+Requires at least: 2.0.0
+Tested up to: 2.8.6
+Stable tag: 1.0.0
+
+This plugin introduces new tag that turns any text between the tags upside down. This is achieved using unicode characters.
+
+== Description ==
+
+This plugin introduces new tag to use in our WordPress documents: &lt;upsidedown&gt;. Any text placed within this tag will be turned 180 degrees and will appear as if it has been turned upside down. It will read just fine, but from right to left. Alternatively, you can turn your monitor upside down and read it.
+
+You might have noticed when quite a few blogs use stokes as if to delete what’s been written before. Do this no more – it’s a thing of the past. Let’s set a new trend by turning the text you want to “hide” upside down.
+
+This looks particularly good when you have a small paragraph reversed. Could be used to type in some nasty details in otherwise “proper” post.
+
+Also if your post raises some question you could use this to provide the answers, but turned upside down.
+
+This does not involve any image manipulation and achieved by replacing characters with corresponding Unicode characters that look like a mirror image of the “normal” symbols.
+
+Please note that ALL characters within these tags will be converted, so you will loose any other HTML tags. Also please note that switching between visual and HTML editor will remove &lt;upsidedown&gt; tags (text in between will not be lost though). So create your post, save it, switch to HTML editor, add tags where necessary, save and publish without switching back to visual editor.
+
+== Installation ==
+
+1. Upload and unpack upside-down-text.zip to `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Enclose any text in your posts with <upsidedown> tags to display it upside down.
+
+== Frequently Asked Questions ==
+
+= Can I use formatting HTML tags within tags?
+
+No, all formatting text will be lost. You can use formatting outside, in which case your flipped text will appear with formatting applied to it.
+
+= Why some symbols are not displayed correctly?
+
+Well, that’s a question to browser developers, I think. Unicode character support is bit flakey in some of them, and not all characters are rendered properly. It also seems to be position and neighbouring characters related too. So far I’ve noticed issues with letter ‘p’, so just try to rephrase your sentences and experiment to get the best result.
+
+== Screenshots ==
+
+1. Text as it appears turned upside-down
+
+== Changelog ==
+
+= 1.0.0 =
+
+* Initial version
---
+++ upside-down-text.php
@@ -0,0 +1,150 @@
+<?php
+/*
+Plugin Name: Upside Down Text
+Plugin URI: http://www.grenadepod.com/projects/upside-down-text-plugin-for-wordpress/
+Description: This plugin allows to "flip" any section of text upside down
+Version: 1.0.0
+Author: Grenadepod
+Author URI: http://www.grenadepod.com
+*/
+
+/*  Copyright 2009 pulegium (email : info@grenadepod.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// Since we're introducing a new attribute (<upsidedown>) we need to 'legalise' it.
+// More info in wp-includes/kses.php
+if (!CUSTOM_TAGS) {
+    // allow new tag in the posts
+    $allowedposttags['upsidedown'] = array();
+    // ... and also in the comments
+    $allowedtags['upsidedown'] = array();
+}
+
+function convert_to_upsidedown_unicode($match)
+{
+    // conversion table
+    $flipTable = array(
+                        '!' => '&#x00A1;',
+                        '"' => '&#x201E;',
+                        '&' => '&#x214B;',
+                        "'" => ',',
+                        '(' => ')',
+                        ')' => '(',
+                        ',' =>  "'",
+                        '.' => '&#x02D9',
+                        '0' => '0',
+                        '1' => '&#x21C2;',
+                        '2' => '&#x1105;',
+                        '3' => '&#x0190;',
+                        '4' => '&#x3123;',
+                        '5' => '&#x078E',
+                        '6' => '9',
+                        '7' => '&#x3125;',
+                        '8' => '8',
+                        '9' => '6',
+                        ';' => '&#x061B;',
+                        '<' => '>',
+                        '>' => '<',
+                        '?' => '&#x00BF',
+                        'A' => '&#x2200',
+                        'B' => '&#x10412',
+                        'C' => '&#x0186',
+                        'D' => '&#x25D6',
+                        'E' => '&#x018E',
+                        'F' => '&#x2132',
+                        'G' => '&#x2141',
+                        'H' => 'H',
+                        'I' => 'I',
+                        'J' => '&#x017F',
+                        'K' => '&#x22CA',
+                        'L' => '&#x02E5',
+                        'M' => 'W',
+                        'N' => 'N',
+                        'O' => 'O',
+                        'P' => '&#x0500',
+                        'Q' => '&#x038C',
+                        'R' => '&#x1D1A',
+                        'S' => 'S',
+                        'T' => '&#x22A5',
+                        'U' => '&#x2229',
+                        'V' => '&#x039B',
+                        'W' => 'M',
+                        'Y' => '&#x2144',
+                        '[' => ']',
+                        ']' => '[',
+                        '_' => '&#x203E',
+                        'a' => '&#x0250',
+                        'b' => 'q',
+                        'c' => '&#x0254',
+                        'd' => 'p',
+                        'e' => '&#x01DD',
+                        'f' => '&#x025F',
+                        'g' => '&#x0183',
+                        'h' => '&#x0265',
+                        'i' => '&#x0131',
+                        'j' => '&#x027E',
+                        'k' => '&#x029E',
+                        'l' => '&#x0283',
+                        'm' => '&#x026F',
+                        'n' => 'u',
+                        'o' => 'o',
+                        'p' => 'd',
+                        'q' => 'b',
+                        'r' => '&#x0279',
+                        's' => 's',
+                        't' => '&#x0287',
+                        'u' => 'n',
+                        'v' => '&#x028C',
+                        'w' => '&#x028D',
+                        'x' => 'x',
+                        'y' => '&#x028E',
+                        'z' => 'z',
+                        '{' => '}',
+                        '}' => '{'
+    );
+
+    // original string needs to be reversed, because we're rotating it 180 degrees
+    // in future releases this might have two options: rotate 180 or mirror (no reverse then)
+    $origStr = strrev($match[1]);
+    $newStr = ' ';
+    for ($i = 0; $i < strlen($origStr); $i++) {
+        $ch = $origStr[$i];
+        if (array_key_exists($ch, $flipTable)) {
+            $newStr .= $flipTable[$ch];
+        } else {
+            $newStr .= $ch;
+        }
+    }
+    return $newStr;
+}
+
+// This is a filter hook function that selects portions of the content in between new tags
+// and passes contents for conversion to character mapping function
+function flip_text_upside_down_filter($content) 
+{
+    return preg_replace_callback(
+        '/\s*<upsidedown>(.*)<\/upsidedown>/siU',
+        'convert_to_upsidedown_unicode',
+        $content
+    );
+}
+
+// And just add appropriate filters where we want to allow upside-down text:
+// post body, excerpt and comments
+add_filter('the_content', 'flip_text_upside_down_filter');
+add_filter('the_excerpt', 'flip_text_upside_down_filter');
+add_filter('comment_text', 'flip_text_upside_down_filter');
